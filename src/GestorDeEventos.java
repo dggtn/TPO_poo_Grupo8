afa4d551361d0741c14c80245c591f29e4cea111
daniela <dggtn97@gmail.com>
2024-10-29 10:30:52 -0300

    agrego metodos a la clase gestorDeEventos

--- src/GestorDeEventos.java
+++ src/GestorDeEventos.java
@@ -1,9 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class GestorDeEventos {
     public static void main(String[] args) {
+        ArrayList<Evento>eventos;
 
+        public void crearEvento(){
 
 
 
         }
+
+        public void editarEvento(Evento evento){
+
+        }
+        public List listarEventosPasados(){
+
+        }
+        public List listarEventosFuturos(){
+
+        }
+        public void mostrarDetalesDelEvento(Evento evento){
+
+        }
+        public void eliminar(Evento evento){
+
+        }
+
     }
