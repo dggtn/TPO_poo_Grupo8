1395cb52cae0588fa4b9043bee584c921866633d
daniela <dggtn97@gmail.com>
2024-10-29 10:10:53 -0300

    escribo clases

---
+++ src/Evento.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Evento {
+    private String descripcion;
+    private String estado;
+    private Date fecha;
+    private String tipo;
+    private int capacidadMaxima;
+    private ArrayList<Persona>integrantes;
+    private ArrayList<Recurso>recursos;
+    private ArrayList<Feedback>feedbacks;
+
+
+    public void escribirFeedback(String mensaje,Persona autor){
+
+    }
+}
---
+++ src/GestorDeEventos.java
@@ -0,0 +1,9 @@
+
+public class GestorDeEventos {
+    public static void main(String[] args) {
+
+
+
+
+        }
+    }
