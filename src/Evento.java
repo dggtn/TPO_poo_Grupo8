8005aa0e326b2b132faa01ddbed80f122fa456bf
daniela <dggtn97@gmail.com>
2024-10-29 10:26:26 -0300

    agrego metodos a la clase Evento

--- src/Evento.java
+++ src/Evento.java
@@ -15,4 +15,34 @@ public class Evento {
     public void escribirFeedback(String mensaje,Persona autor){
 
     }
+
+    public Evento(String descripcion, String estado, Date fecha, String tipo, int capacidadMaxima) {
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.fecha = fecha;
+        this.tipo = tipo;
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public void asignarRecursos(String nombre){
+
+    }
+
+    public void mostrarParticipantes(){
+
+    }
+
+    public void agregarIntegrante(Persona Integrante){
+
+    }
+    public void borrarParticipantes(Persona Integrante){
+
+    }
+
+    public void mostrarEstadoDelEvento(){
+
+    }
+    public void cancelarEvento(){
+
+    }
 }
